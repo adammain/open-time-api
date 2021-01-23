383abaa759dd6aa07c0331d162e0d030043d5da0
Adam Main <northforth@me.com>
2021-01-22 19:00:52 -0700

    Updated: CORS middleware origin config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
+  CLIENT_ORIGIN: 'https://dry-lowlands-01034.herokuapp.com',
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN
 }
