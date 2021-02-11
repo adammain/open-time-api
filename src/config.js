7d8b90440bb0d6a2c96739c2e38a3f07197e379f
Adam Main <northforth@me.com>
2021-02-10 20:50:16 -0700

    Fix: CORS Error

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  CLIENT_ORIGIN: 'http://localhost:3000/',
+  CLIENT_ORIGIN: 'http://localhost:3000',
   // CLIENT_ORIGIN: 'http://localhost:3000',
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN,
